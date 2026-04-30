8ebf20b0d090d71924713b60a39d6245f5bb66c9
Simon Walther <simon.walt@hotmail.com>
2026-04-30 16:20:09 +0200

    Add leaflet map

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,39 @@
 <template>
-  <div></div>
+  <div id="map"></div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+})
+
+onMounted(() => {
+  const map = L.map('map').setView([46.5197, 7.0], 13)
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors'
+  }).addTo(map)
+})
+</script>
+
+<style scoped>
+#map {
+  height: 100vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+:deep(.leaflet-container) {
+  font: inherit;
+}
+</style>
